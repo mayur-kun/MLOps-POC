de49c6d67936fdf59988bca79d2418881e361969
AmmarKhan1209 <69052500+AmmarKhan1209@users.noreply.github.com>
2023-05-15 11:09:24 +0000

    lin regression model is fed into model.py file

--- Model.py
+++ Model.py
@@ -1,53 +1,19 @@
 '''
 Project - MLOps_POC
-Image Classifier using Keras
+Salary Prediction
 '''
 
-import tensorflow as tf
-from tensorflow.keras import layers
-from tensorflow.keras import Model
-from tensorflow.keras.optimizers import RMSprop
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import numpy as np
 
-# Define the model architecture
-input_layer = layers.Input(shape=(150, 150, 3))
-x = layers.Conv2D(32, (3, 3), activation='relu')(input_layer)
-x = layers.MaxPooling2D(2, 2)(x)
-x = layers.Conv2D(64, (3, 3), activation='relu')(x)
-x = layers.MaxPooling2D(2, 2)(x)
-x = layers.Conv2D(128, (3, 3), activation='relu')(x)
-x = layers.MaxPooling2D(2, 2)(x)
-x = layers.Conv2D(128, (3, 3), activation='relu')(x)
-x = layers.MaxPooling2D(2, 2)(x)
-x = layers.Flatten()(x)
-x = layers.Dense(512, activation='relu')(x)
-output_layer = layers.Dense(1, activation='sigmoid')(x)
+location = "POC-data.xlsx"
+df = pd.read_excel(location)
 
-# Define the model object and compile it
-model = Model(input_layer, output_layer)
-model.compile(loss='binary_crossentropy',
-              optimizer=RMSprop(lr=1e-4),
-              metrics=['accuracy'])
+X = df.drop("Salary",axis = 1)
+y = df["Salary"]
 
-# Load the data
-train_dir = '/path/to/training/data/'
-validation_dir = '/path/to/validation/data/'
+model = LinearRegression()
+model.fit(X,y)
 
-train_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1.0/255)
-train_generator = train_datagen.flow_from_directory(train_dir,
-                                                    batch_size=20,
-                                                    class_mode='binary',
-                                                    target_size=(150, 150))
-
-validation_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1.0/255)
-validation_generator = validation_datagen.flow_from_directory(validation_dir,
-                                                              batch_size=20,
-                                                              class_mode='binary',
-                                                              target_size=(150, 150))
-
-# Train the model
-history = model.fit_generator(train_generator,
-                              validation_data=validation_generator,
-                              steps_per_epoch=100,
-                              epochs=15,
-                              validation_steps=50,
-                              verbose=1)
+print(model.predict(np.array([10,10]).reshape(1,-1)))
